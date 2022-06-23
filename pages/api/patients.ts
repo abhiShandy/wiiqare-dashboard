d96c3a841137f61cdb9048ed44a746358ab7bb0a
Abhishek Shandilya <ashandilya@greenactionstudio.com>
2022-06-22 23:41:08 -0400

    create customer

---
+++ pages/api/patients.ts
@@ -0,0 +1,31 @@
+import { MongoClient } from "mongodb";
+import { NextApiHandler } from "next";
+import { Patient } from "../customers";
+import { v4 as uuid } from "uuid";
+
+const Patients: NextApiHandler = async (request, response) => {
+  if (request.method === "POST") {
+    const { name, email } = request.body;
+
+    const client = new MongoClient(process.env.MONGODB_URL || "");
+
+    try {
+      await client.connect();
+    } catch (error) {
+      console.log("Error connecting to MongoDB");
+    }
+
+    try {
+      const data = await client
+        .db("customers")
+        .collection<Patient>("patients")
+        .insertOne({ id: uuid(), name, email });
+      response.status(200).json(data);
+    } catch (error) {
+      console.log("Error creating Patient!");
+      response.status(500);
+    }
+  }
+};
+
+export default Patients;
--- pages/customers.tsx
+++ pages/customers.tsx
@@ -112,6 +112,98 @@ const CreateExpatModal = () => {
   );
 };
 
+const CreatePatientModal = () => {
+  const [isOpen, setIsOpen] = useState(false);
+  const [email, setEmail] = useState("");
+  const [name, setName] = useState("");
+
+  function closeModal() {
+    setIsOpen(false);
+  }
+
+  function openModal() {
+    setIsOpen(true);
+  }
+
+  async function createPatient() {
+    try {
+      await axios.post("/api/patients", { name, email });
+      closeModal();
+    } catch (error) {
+      console.log("Could not create patient");
+    }
+  }
+
+  return (
+    <Modal
+      buttonText="Create Patient"
+      isOpen={isOpen}
+      openModal={openModal}
+      closeModal={closeModal}
+    >
+      <Dialog.Title
+        as="h3"
+        className="text-lg font-medium leading-6 text-gray-900"
+      >
+        Create a patient profile
+      </Dialog.Title>
+
+      <div className="mt-2">
+        <form>
+          <div className="flex flex-col my-2">
+            <label htmlFor="email" className="my-1">
+              Email
+            </label>
+            <input
+              type="email"
+              name="email"
+              className="border rounded h-10 px-4 py-2"
+              placeholder="test@example.com"
+              value={email}
+              onChange={(e) => {
+                setEmail(e.target.value);
+              }}
+            />
+          </div>
+
+          <div className="flex flex-col my-2">
+            <label htmlFor="name" className="my-1">
+              Name
+            </label>
+            <input
+              type="text"
+              name="name"
+              className="border rounded h-10 px-4 py-2"
+              placeholder="John Doe"
+              value={name}
+              onChange={(e) => {
+                setName(e.target.value);
+              }}
+            />
+          </div>
+        </form>
+      </div>
+
+      <div className="mt-4 flex justify-between">
+        <button
+          type="button"
+          className="inline-flex justify-center rounded-md border border-transparent bg-indigo-100 px-4 py-2 text-sm font-medium text-indigo-900 hover:bg-indigo-200 focus:outline-none focus-visible:ring-2 focus-visible:ring-indigo-500 focus-visible:ring-offset-2"
+          onClick={createPatient}
+        >
+          Create
+        </button>
+        <button
+          type="button"
+          className="inline-flex justify-center rounded-md border border-indigo-100 px-4 py-2 text-sm font-medium text-indigo-900 focus:outline-none focus-visible:ring-2 focus-visible:ring-indigo-500 focus-visible:ring-offset-2"
+          onClick={closeModal}
+        >
+          Cancel
+        </button>
+      </div>
+    </Modal>
+  );
+};
+
 const Customers = ({ expats, patients }: Props) => {
   return (
     <>
@@ -146,7 +238,10 @@ const Customers = ({ expats, patients }: Props) => {
           </table>
         </div>
 
-        <h2 className="text-xl font-bold m-4">Patients</h2>
+        <div className="flex flex-row justify-between m-4">
+          <h2 className="text-xl font-bold">Patients</h2>
+          <CreatePatientModal />
+        </div>
         <div className="border border-gray-200 rounded-md">
           <table className="w-full">
             <thead>
