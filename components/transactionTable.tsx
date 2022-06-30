431c7c619649e4f7b4c175c925ca9bb21761e8d0
Abhishek Shandilya <ashandilya@greenactionstudio.com>
2022-06-30 00:18:47 -0400

    rearrange table columns

--- components/transactionTable.tsx
+++ components/transactionTable.tsx
@@ -1,15 +1,12 @@
-import { Transaction } from "./transactions";
-
 export default function TransactionTable({
   transactions,
 }: {
-  transactions?: Transaction[];
+  transactions?: WiiQare.Transaction[];
 }) {
   return (
     <>
       <div className="px-4 sm:px-6 lg:px-8">
-        <div className="mt-8 flex flex-col">
-          <p className="py-2 text-xl">Transactions</p>
+        <div className="flex flex-col">
           {transactions && (
             <div className="-my-2 -mx-4 overflow-x-auto sm:-mx-6 lg:-mx-8">
               <div className="inline-block min-w-full py-2 align-middle md:px-6 lg:px-8">
@@ -19,22 +16,17 @@ export default function TransactionTable({
                       <tr>
                         <th
                           scope="col"
-                          className="py-3.5 pl-4 pr-3 text-left text-sm font-semibold text-gray-900 sm:pl-6"
-                        >
-                          Amount
-                        </th>
-                        <th
-                          scope="col"
-                          className="px-3 py-3.5 text-left text-sm font-semibold text-gray-900"
-                        >
-                          Date Created
-                        </th>
-                        <th
-                          scope="col"
-                          className="px-3 py-3.5 text-left text-sm font-semibold text-gray-900"
+                          className="px-3 pl-4 pr-3 text-left text-sm font-semibold text-gray-900 sm:pl-6"
                         >
                           Last updated
                         </th>
+                        <th
+                          scope="col"
+                          className="py-3.5 pl-4 pr-3 text-left text-sm font-semibold text-gray-900"
+                        >
+                          Amount
+                        </th>
+
                         <th
                           scope="col"
                           className="px-3 py-3.5 text-left text-sm font-semibold text-gray-900"
@@ -46,7 +38,10 @@ export default function TransactionTable({
                     <tbody className="divide-y divide-gray-200 bg-white">
                       {transactions.map((transaction) => (
                         <tr key={transaction.hash}>
-                          <td className="whitespace-nowrap py-4 pl-4 pr-3 text-sm font-medium text-gray-900 sm:pl-6">
+                          <td className="whitespace-nowrap px-3 py-4 text-sm text-gray-500 sm:pl-6">
+                            {new Date(transaction.lastUpdated).toLocaleString()}
+                          </td>
+                          <td className="whitespace-nowrap py-4 pl-4 pr-3 text-sm text-gray-900">
                             <div>
                               {transaction.paidAmount}{" "}
                               {transaction.paidCurrency}
@@ -56,14 +51,9 @@ export default function TransactionTable({
                               {transaction.displayCurrency}
                             </div>
                           </td>
+
                           <td className="whitespace-nowrap px-3 py-4 text-sm text-gray-500">
-                            {new Date(transaction.dateCreated).toLocaleString()}
-                          </td>
-                          <td className="whitespace-nowrap px-3 py-4 text-sm text-gray-500">
-                            {new Date(transaction.lastUpdated).toLocaleString()}
-                          </td>
-                          <td className="whitespace-nowrap px-3 py-4 text-sm text-gray-500">
-                            00
+                            {transaction.balance} {transaction.displayCurrency}
                           </td>
                         </tr>
                       ))}
