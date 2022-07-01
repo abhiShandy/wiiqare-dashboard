83b2808eda1ddf54ebd41351cf1021a3fd870563
Abhishek Shandilya <ashandilya@greenactionstudio.com>
2022-06-30 22:05:36 -0400

    add button type

--- components/button.tsx
+++ components/button.tsx
@@ -3,6 +3,7 @@ import React from "react";
 export default function Button(props: React.ButtonHTMLAttributes<any>) {
   return (
     <button
+      type={props.type || "button"}
       className={`inline-flex justify-center rounded-md border border-transparent bg-indigo-100 px-4 py-2 text-sm font-medium text-indigo-900 ${props.className}`}
       onClick={props.onClick}
     >
