d0ed1f3e0d40f4933c79cc9a47646b17aeec89a7
Abhishek Shandilya <ashandilya@greenactionstudio.com>
2022-06-30 22:17:17 -0400

    change dashboard title position

--- components/dashboard.tsx
+++ components/dashboard.tsx
@@ -227,7 +227,7 @@ export default function Dashboard({
           <main className="flex-1">
             <div className="py-6">
               <div className="max-w-7xl mx-auto px-4 sm:px-6 md:px-8">
-                <h1 className="text-2xl font-semibold text-gray-900 mb-2">
+                <h1 className="text-2xl font-semibold text-gray-900 mb-4 px-4 sm:px-6 lg:px-8">
                   {title}
                 </h1>
               </div>
